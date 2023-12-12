e91d0b2d48421ede529656b6233e9a246b7cc940
Gabriel Oliveira Santos <gabriel.santos@sede.telebras>
2023-12-12 14:38:08 -0300

    Exercicio1

---
+++ Modulo1/Boolean.js
@@ -0,0 +1,16 @@
+const valor1 = true;
+const valor2 = false;
+
+const valor1EValor2 = valor1 && valor2;
+const valor1OUValor2 = valor1 || valor2;
+const inversoValor1 = !valor1;
+const inversoValor2 = !valor2;
+
+console.log(valor1EValor2);
+//false
+console.log(valor1OUValor2);
+//true
+console.log(inversoValor1);
+//false
+console.log(inversoValor2);
+//true
