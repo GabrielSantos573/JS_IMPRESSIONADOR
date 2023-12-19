d8c4736be9ae4aede0a4300aef19f235242fb358
Gabriel Oliveira Santos <80365261+GabrielSantos573@users.noreply.github.com>
2023-12-19 16:54:39 -0300

    Delete Modulo1 directory

--- Modulo1/Exercicio1.js
+++
@@ -1,9 +0,0 @@
-const val1 = 2148
-let valor2
-
-console.log(`O valor da primeira é ${val1}`)
-
-valor2 = 15
-
-console.log(`O valor da segunda é ${valor2} e o valor da soma é ${val1+valor2}`)
-
--- Modulo1/TESTE.js
+++
@@ -1,7 +0,0 @@
-const msg = 'JAVASCRIPT!=!'; //const n pode ser alterado e let pode ser 
-
-console.log(msg);
-
-msg = 'testeteste';
-
-console.log(msg);
